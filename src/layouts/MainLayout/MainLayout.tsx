2ca80e40d50147d9797994fc3d3692c7ad25455d
Pablo Monteiro <paabloms@live.com>
2025-12-29 12:27:30 -0300

    Modificação no Menu (#26) (#28)
    * Layout modificado para a parte da esquerda deslizar para preencher espaço vazio
    
    * Erros corrigidos
    
    Co-authored-by: Lethycia Zenaide <lethyciazenaide@gmail.com>

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -2,11 +2,14 @@ import { Outlet } from 'react-router-dom';
 import { Sidebar } from '../Sidebar';
 import { Header } from '../Header';
 import styles from './MainLayout.module.css';
+import { useState } from 'react';
 
 export function MainLayout() {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
     return (
-        <div className={styles.layout}>
-            <Sidebar />
+        <div className={`${styles.layout} ${isCollapsed ? styles.collapsed : ''}`}>
+            <Sidebar isCollapsed={isCollapsed} onToggle={() => setIsCollapsed((s) => !s)} />
             <div className={styles.main}>
                 <Header />
                 <main className={styles.content}>
