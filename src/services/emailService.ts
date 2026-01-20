385cd874608212cbd11b6291ff4f50074ef49804
Horlanlacerda <horlansilva56@gmail.com>
2026-01-20 17:06:50 -0300

    feat(US16): cria as credenciais e o construtor de inicialização

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -7,3 +7,13 @@ export interface BoletoFormData{
     vencimento: string;
     descricao: string;
 }
+
+export default class EmailService{
+    private SERVICE_ID = 'service_fzi27sq';
+    private TEMPLATE_ID = 'template_m73prwg';
+    private PUBLIC_KEY = 'DM2LYUOXZVvRjO2bl';
+
+    constructor(){
+        emailjs.init(this.PUBLIC_KEY);
+    }
+}
