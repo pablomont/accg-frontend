22c6883f379741e221842d3c874d6215d4624a0f
Horlanlacerda <horlansilva56@gmail.com>
2026-01-20 20:17:09 -0300

    feat(US16): adiciona a função assíncrona para envio do email

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,6 +1,7 @@
-import emailjs from '@emailjs/browser'
-import { formatCurrency } from '@/utils'
-import { BoletoFormData } from '@/types';
+import emailjs from '@emailjs/browser';
+import { formatCurrency } from '@/utils';
+import { formatDate } from '@/utils';
+import { Boleto } from '@/types';
 
 
 export default class EmailService{
@@ -10,5 +11,17 @@ export default class EmailService{
 
     constructor(){
         emailjs.init(this.PUBLIC_KEY);
+    }
+
+    // async/await --> JavaScript Assíncrono: garante um tempo de espera com uma promessa (Promise) de que algo será executado.
+    public async enviaCobranca(dados: Boleto){
+        const template = {
+            nome: dados.associado?.nome,
+            valor: formatCurrency(dados.valor),
+            data_vencimento: formatDate(dados.dataVencimento),
+            descricao: dados.descricao,
+        };
+
+        return emailjs.send(this.SERVICE_ID, this.TEMPLATE_ID, template);
     }
 }
